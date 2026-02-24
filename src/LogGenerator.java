a15fc677d7c0f23f784cfee6ef7e4f18ebf25aae
Keerthi <keerthipadamati16@gmail.com>
2026-02-24 17:20:42 +0530

    Enhance LogGenerator to create dynamic logs based on size

--- src/LogGenerator.java
+++ src/LogGenerator.java
@@ -1,21 +1,44 @@
 import java.io.*;
+import java.nio.*;
+import java.nio.channels.FileChannel;
 import java.nio.file.*;
+import java.util.Scanner;
 
 public class LogGenerator
 {
     public static void main(String[] args) throws IOException
     {
-        String file = "test.log";
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Size in GB: "); 
+        long gb = sc.nextLong();
+        long target = gb * 1024 * 1024 * 1024;
         
-        String line = "INFO 2026-02-19 User action\n";
         
-        String error = "SEVERE 2026-02-20 CRITICAL_ERROR\n";
-        
-        try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(file)))
+        if (target > new File(".").getUsableSpace())
         {
-            for (int i = 0; i < 1000000; i++)
-                writer.write(i % 50000 == 0 ? error : line);
+            System.out.println("No Space!"); 
+            return; 
         }
-        System.out.println("Log Created.");
+
+        try(FileChannel out = FileChannel.open(Paths.get("test.log"), StandardOpenOption.CREATE, StandardOpenOption.WRITE))
+        {
+            ByteBuffer buf = ByteBuffer.allocate(8 * 1024 * 1024);
+            byte[] line = "INFO 2026-02-19 CRITICAL_ERROR: Failure\n".getBytes();
+            long current = 0;
+            while (current < target)
+            {
+                if (buf.remaining() < line.length)
+                { 
+                    buf.flip(); 
+                    out.write(buf); 
+                    buf.clear(); 
+                }
+                buf.put(line); 
+                current += line.length;
+            }
+            buf.flip(); 
+            out.write(buf);
+        }
+        System.out.println("Dynamic Log Created.");
     }
 }
